9486074b67c4ac90676c82ad3be00ce0a928874b
JulienFichot <117909465+JulienFichot@users.noreply.github.com>
2023-02-12 16:37:38 +0100

    Final

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,9 +5,32 @@ function login() {
     email: document.getElementById("signin-email"),
     password: document.getElementById("signin-password"),
   };
+  function createDiv() {
+    // Création de la div
+    var div = document.createElement("div");
+    div.style.backgroundColor = "black";
+    div.style.color = "white";
+    div.style.padding = "20px";
+
+    // Ajout de texte à la div
+    var text = document.createTextNode("Bienvenue sur mon site web !");
+    div.appendChild(text);
+
+    // Ajout d'un bouton
+    var button = document.createElement("button");
+    button.innerHTML = "Cliquez ici";
+    div.appendChild(button);
+
+    // Ajout de la div au document
+    document.body.appendChild(div);
+  }
 
   button.addEventListener("click", (e) => {
+    createDiv;
+
     e.preventDefault();
+
+    //localhost:5678/api/works/IDPROJET
 
     fetch("http://localhost:5678/api/users/login", {
       method: "POST",
@@ -22,14 +45,23 @@ function login() {
     })
       .then((response) => {
         if (response.ok) {
-          window.open("index.html");
+          return response.json();
         } else if (response.status === 404) {
-          error.innerHTML = "Csette adresse mail est inconnue.";
+          error.innerHTML = "Cette adresse mail est inconnue.";
           error.removeAttribute("hidden");
         } else if (response.status === 401) {
           error.innerHTML = "Veuillez vérifier votre mot de passe.";
           error.removeAttribute("hidden");
         }
+        return false;
+      })
+      .then((data) => {
+        if (data) {
+          localStorage.setItem("token", data.token);
+          window.location = "index.html";
+          console.log(data.token);
+        }
+        console.log(data);
       })
       .catch((err) => {
         console.log(err);
